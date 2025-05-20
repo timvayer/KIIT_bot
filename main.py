25ac07161234e6d18ca2f7861b2098b64d04f19c
timvayer <royjohnsony@gmail.com>
2025-05-20 19:23:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import telebot
 from flask import Flask, request
 
-TOKEN = "7760817892:AAGAVTC5EZs_TJzhIkYuuXD9tvdTCo_bIKc"  # заміни на свій токен  # заміни на свій токен
-bot = telebot.TeleBot(TOKEN)
+TOKEN = "ТВОЙ_ТОКЕН"
+bot = telebot.TeleBot(TOKEN, threaded=False)
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])  # зміна тут
+@app.route('/webhook', methods=['POST'])
 def webhook():
     if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
+        json_str = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_str)
         bot.process_new_updates([update])
         return '', 200
     else:
@@ -18,7 +18,7 @@ def webhook():
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привіт! Я працюю.')
+    bot.send_message(message.chat.id, "Привіт! Я працюю.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
@@ -29,4 +29,6 @@ def index():
     return 'KIIT_bot is running'
 
 if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url="https://kiit-bot.onrender.com/webhook")
     app.run(host='0.0.0.0', port=10000)
