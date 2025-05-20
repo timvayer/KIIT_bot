e3565659da923698b1726e8f03a1d8da3b9b9195
timvayer <royjohnsony@gmail.com>
2025-05-20 19:05:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import telebot
-import os
 from flask import Flask, request
 
-TOKEN = os.environ.get('TELEGRAM_TOKEN')
+TOKEN = 7760817892:AAGAVTC5EZs_TJzhlkYuuXD9tvdTCo_bIKc  # заміни на свій токен
 bot = telebot.TeleBot(TOKEN)
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return 'KIIT_bot is running', 200
-
-@app.route('/', methods=['POST'])
-def receive_update():
-    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
-    bot.process_new_updates([update])
-    return '', 200
+@app.route('/webhook', methods=['POST'])  # зміна тут
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return '', 200
+    else:
+        return 'Invalid content type', 403
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привіт! Я працюю.")
+    bot.send_message(message.chat.id, 'Привіт! Я працюю.')
 
 @bot.message_handler(func=lambda message: True)
-def echo(message):
+def echo_all(message):
     bot.send_message(message.chat.id, f"Ти сказав: {message.text}")
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'KIIT_bot is running'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
