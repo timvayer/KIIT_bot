6e81c4062ff99d94ecc434a5a017302c0ab469fa
timvayer <royjohnsony@gmail.com>
2025-05-20 17:57:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,5 @@ def echo_all(message):
     bot.reply_to(message, message.text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
