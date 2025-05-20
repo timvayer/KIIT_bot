13f7e484e1a54334eb29d7ae88b5ce9fa5812bb6
timvayer <royjohnsony@gmail.com>
2025-05-20 18:51:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+import telebot
 import os
 from flask import Flask, request
-import telebot
 
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '!', 200
-
 @app.route('/', methods=['GET'])
-def index():
+def home():
     return 'KIIT_bot is running', 200
 
+@app.route('/', methods=['POST'])
+def receive_update():
+    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+    bot.process_new_updates([update])
+    return '', 200
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привіт! Я бот кавʼярні КІІТ.")
+def start_message(message):
+    bot.send_message(message.chat.id, "Привіт! Я працюю.")
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def echo(message):
+    bot.send_message(message.chat.id, f"Ти сказав: {message.text}")
